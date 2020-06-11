b82241a446c5719f4632c989ad690396c6005c6a
Choi Jiwon <winning1993@naver.com>
2020-06-12 07:21:51 +0900

    API 데이터 불러오기 성공

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-const searchForm = document.getElementById("searchForm")
+const searchForm = document.getElementById("searchForm"),
+searchInput = document.getElementById("searchInput"),
+resultArea = document.getElementById("resultArea")
+
+
 
 async function handleSearch(e){
     e.preventDefault();
-    const x = await fetch("https://api.audd.io/findLyrics/?q=adele hello").then((res)=>{
-        console.log(res.json())
-    });
+    const keyword = searchInput.value;
+    const data = {
+        'url': `https://api.audd.io/findLyrics/?q=${keyword}`,
+        'return': 'apple_music,spotify',
+        'api_token': '5c8d51bfb6dc74c47030d4092128a69b'
+    }
+    axios.get(data.url,{
+        params:{
+            api_token: data.api_token
+        }
+    }).then((res) => {
+        console.log(res);
+        handleApi(res)
+        
+    })
+}
+
+function handleApi(res){
+    console.log(res)
+    const songs = res.data.result;
+    resultArea.innerHTML = songs[1].full_title;
 }
 
 function init(){
